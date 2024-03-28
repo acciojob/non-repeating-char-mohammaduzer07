a92290d0884bc62a95afc79ed6af1d9bc8b5eb8b
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-03-28 17:21:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+
+	let i =0;          
+	let j =0;
+         
+	while(i < str.length){
+	  if(str[i] === str[j]){
+	    i++;
+	  }
+	  else{
+	    if(i-j <= 1){
+	      console.log(str[j]);
+	      break;
+	    }
+	    j=i;
+	    i++;
+	  }
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
