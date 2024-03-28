2f54aa795325608f6312095a05c2e4adc06958df
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-03-28 17:25:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ function firstNonRepeatedChar(str) {
 	let j =0;
          
 	while(i < str.length){
-	  if(str[i] === str[j]){
+	  if(str.charAt(i) === str.charAt(j)){
 	    i++;
 	  }
 	  else{
 	    if(i-j <= 1){
-	      console.log(str[j]);
+	      console.log(str.charAt(j));
 	      break;
 	    }
 	    j=i;
 	    i++;
-	  }
+	  }      
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
