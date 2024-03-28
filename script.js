3f9bcedc1f2f82be2e9f89f402e4493e3d8e3fd9
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-03-28 17:36:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
 
-	let i =0;          
-	let j =0;
+	// let i =0;          
+	// let j =0;
          
-	while(i < str.length){
-	  if(str.charAt(i) === str.charAt(j)){
-	    i++;
-	  }
-	  else{
-	    if(i-j <= 1){
-	      return (str.charAt(j));
-	    }
-	    j=i;
-	    i++;
-	  }     
-		return null
+	// while(i < str.length){
+	//   if(str.charAt(i) === str.charAt(j)){
+	//     i++;
+	//   }
+	//   else{
+	//     if(i-j <= 1){
+	//       return (str.charAt(j));
+	//     }
+	//     j=i;
+	//     i++;
+	//   }     
+	// 	return null
+	if (!str) return null;
+
+  const charCount = {};
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str.charAt(i);
+    charCount[char] = charCount[char] ? charCount[char] + 1 : 1;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str.charAt(i);
+    if (charCount[char] === 1) {
+      return char; // Return the first non-repeated character
+    }
+  }
+
+  return null; // If no non-repeated character is found 
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
